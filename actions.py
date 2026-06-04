bbe694f4abea39d268e53ce384ac3dc3af12b5ea
Tanuj Gupta <tanujgupta0602@gmail.com>
2026-06-05 01:13:18 +0530

    add mouse control

--- actions.py
+++ actions.py
@@ -1,5 +1,7 @@
 import comtypes
 import screen_brightness_control as sbc
+import pyautogui
+import numpy as np
 
 class VolumeController:
     def __init__(self):
@@ -30,3 +32,19 @@ class BrightnessController:
         else:
             new = max(brightness - step, 0)
         sbc.set_brightness(new)
+
+class MouseController:
+    def __init__(self):
+        self.screen_w=1920
+        self.screen_h=1080
+        self.prev_x = 0
+        self.prev_y = 0
+        self.smoothing = 5
+
+    def move(self, x,y ,frame_w, frame_h):
+        screen_x = np.interp(x, [100, frame_w-100], [0, 1920])
+        screen_y = np.interp(y, [100, frame_h-100], [0, 1080])
+        curr_x = self.prev_x  + (screen_x - self.prev_x) / self.smoothing
+        curr_y = self.prev_y + (screen_y - self.prev_y) / self.smoothing
+        pyautogui.moveTo(curr_x, curr_y)
+        self.prev_x, self.prev_y = curr_x, curr_y
--- main.py
+++ main.py
@@ -4,13 +4,18 @@ from detector import HandDetector
 from gestures import GestureDetector
 from actions import VolumeController
 from actions import BrightnessController
+from actions import MouseController
 import comtypes
+import pyautogui
 comtypes.CoInitialize()
+pyautogui.PAUSE = 0
+pyautogui.FAILSAFE = False
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 vol_con = VolumeController()
 detector = HandDetector()
 bri_con = BrightnessController()
+mouse = MouseController()
 prev_distance=0
 threshold=5
 
@@ -22,6 +27,7 @@ while True:
             continue
         frame = cv2.flip(frame, 1)
         frame = detector.find_hands(frame)
+        frame_h, frame_w,_ = frame.shape
         landmarks = detector.get_landmarks(frame)
         if landmarks:
             gesture = GestureDetector(landmarks)
@@ -40,6 +46,11 @@ while True:
                 elif distance < prev_distance - threshold:
                     bri_con.change_brightness("down")
                 prev_distance=distance
+            if fingers == [0,1,0,0,0]:
+                x,y = landmarks[8][1], landmarks[8][2]
+                print("moving to:", x, y)
+                mouse.move(x, y, frame_w, frame_h)
+                print("moved")
         cv2.imshow("Gesture Control", frame)
         if cv2.waitKey(10) & 0xFF == ord('q'):
             print("break: q pressed")
